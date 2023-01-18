c8efdff37d1a055cfc89bc06bc3a2c675a0e3e29
ahmedsujon <sujonahmed424@gmail.com>
2023-01-18 13:36:08 +0600

    dark mode statement

--- code.ts
+++ code.ts
@@ -1,33 +1,63 @@
-// This plugin will open a window to prompt the user to enter a number, and
-// it will then create that many rectangles on the screen.
-
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs in the <script> tag inside "ui.html" which has a
-// full browser environment (see documentation).
-
-// This shows the HTML page in "ui.html".
 figma.showUI(__html__);
 
-// Calls to "parent.postMessage" from within the HTML page will trigger this
-// callback. The callback will be passed the "pluginMessage" property of the
-// posted message.
-figma.ui.onmessage = msg => {
-  // One way of distinguishing between different types of messages sent from
-  // your HTML page is to use an object with a "type" property like this.
-  if (msg.type === 'create-rectangles') {
-    const nodes: SceneNode[] = [];
-    for (let i = 0; i < msg.count; i++) {
-      const rect = figma.createEllipse();
-      rect.x = i * 150;
-      rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-      figma.currentPage.appendChild(rect);
-      nodes.push(rect);
+figma.ui.resize(500, 500);
+
+figma.ui.onmessage = (pluginMessage) => {
+
+  const postComponentSet = figma.root.findOne(
+    (node) => node.type == "COMPONENT_SET" && node.name == "post"
+  ) as ComponentSetNode;
+
+  let selectedVariant;
+
+  console.log(pluginMessage.imageVariant);
+
+  if (pluginMessage.darkModeState === true) {
+    switch (pluginMessage.imageVariant) {
+      case "2":
+        selectedVariant = postComponentSet.findOne(
+          (node) =>
+            node.type == "COMPONENT" &&
+            node.name == "Image=single, Dark mode=true"
+        ) as ComponentNode;
+        break;
+      case "3":
+        selectedVariant = postComponentSet.findOne(
+          (node) =>
+            node.type == "COMPONENT" &&
+            node.name == "Image=carousel, Dark mode=true"
+        ) as ComponentNode;
+        break;
+      default:
+        selectedVariant = postComponentSet.findOne(
+          (node) =>
+            node.type == "COMPONENT" &&
+            node.name == "Image=none, Dark mode=true"
+        ) as ComponentNode;
+        break;
     }
-    figma.currentPage.selection = nodes;
-    figma.viewport.scrollAndZoomIntoView(nodes);
+  } else {
+    switch (pluginMessage.imageVariant) {
+      case "2":
+        selectedVariant = postComponentSet.findOne(
+          (node) =>
+            node.type == "COMPONENT" &&
+            node.name == "Image=single, Dark mode=false"
+        ) as ComponentNode;
+        break;
+      case "3":
+        selectedVariant = postComponentSet.findOne(
+          (node) =>
+            node.type == "COMPONENT" &&
+            node.name == "Image=carousel, Dark mode=false"
+        ) as ComponentNode;
+        break;
+      default:
+        selectedVariant = postComponentSet.defaultVariant as ComponentNode;
+        break;
+    }
   }
 
-  // Make sure to close the plugin when you're done. Otherwise the plugin will
-  // keep running, which shows the cancel button at the bottom of the screen.
+  selectedVariant.createInstance();
   figma.closePlugin();
 };
